f489f4c0689df3e685325c04fa5df82cc98602c4
Ludwik Lejzer <ludwiklejzercc@gmail.com>
2022-12-21 22:05:51 -0300

    Turn code POSIX compliant

--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Arch Linux Post-Install Script
-# https://github.com/Lajck/dotfiles/post-installer
-# by Lajck
+# https://github.com/ludwiklejzer/.dotfiles/post-installer
+# by Ludwik Lejzer
 
 # Stop the script in case of some error
-set -euo pipefail
+set -eu
 
 # Text properties
 GREEN=$(tput setaf 2)
@@ -14,125 +14,56 @@ RED=$(tput setaf 1)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
 
-# System support
-if [ "$(uname -n)" != archlinux ]; then
-	clear
-	echo 'Sorry, your system is not supported.'
-	exit 1
-fi
+# # System support
+# if [ "$(uname -n)" != archlinux ]; then
+# 	clear
+# 	echo 'Sorry, your system is not supported.'
+# 	exit 1
+# fi
 
-clear
-echo "Testing internet connection"
-if [ "$(ping -c 1 -q google.com > /dev/null; echo $?)" != 0 ]; then
-	echo "${RED}Please, connect to some network${RESET}"
-	exit 1
-fi
-
-pacman_packages=(
-	alacritty
-	android-tools
-	feh
-	git
-	xorg
-	xorg-xinit
-	qemu
-	bspwm
-	sxhkd
-	os-prober
-	ntfs-3g
-	zsh
-	pipewire
-	pipewire-pulse
-	wireplumber
-	pulsemixer
-	exa
-	jq
-	stow
-	ffmpeg
-	rofi
-	unclutter
-	neofetch
-	neovim
-	fzf
-	libnotify
-	dunst
-	qbittorrent
-	mpv
-	ueberzug
-	redshift
-	zathura
-	zathura-djvu
-	zathura-pdf-mupdf
- 	noto-fonts-emoji
- 	noto-fonts-cjk
-	ranger
-	p7zip
-	unzip
-	zip
-	unrar
-	htop
-	xsel
-	the_silver_searcher
-	ripgrep-all
-	z
-	python-pywal
-	polybar
-	youtube-dl)
-
-aur_packages=(
-	pick-colour-picker
-	asdf-vm
-	picom-animations-git
-	jmtpfs
-	pandoc-bin)
-
-npm_global_packages=(
-	vscode-langservers-extracted
-	cssmodules-language-server
-	bash-language-server
-	prettier)
+# clear
+# echo "Testing internet connection"
+# if [ "$(
+# 	ping -c 1 -q google.com >/dev/null
+# 	echo $?
+# )" != 0 ]; then
+# 	echo "${RED}Please, connect to some network${RESET}"
+# 	exit 1
+# fi
 
 pacman_install() {
 	clear
 
 	# Root privileges test.
-	if [ "$UID" != 0 ]; then
+	if [ "$(id -u)" != 0 ]; then
 		echo "${RED}To install packages from Pacman you must execute this script with sudo privileges${RESET}"
 		exit 1
 	fi
 
-	echo "${BOLD}Updating package list...${RESET}"
-	pacman -Sy --noconfirm > /dev/null 
-	sleep 1
-
-	echo "${BOLD}Upgrading system packages...${RESET}"
-	pacman -Su --noconfirm > /dev/null
-	sleep 1
-
-	echo "${BOLD}Installing Pacman packages...${RESET}"
-	pacman -S "${pacman_packages[@]}" --noconfirm > /dev/null
-	sleep 1
+	pacman -Syu --noconfirm
+	jq -r '.pacman[]' software_list.json | pacman -S - --needed
 }
 
 aur_install() {
+	clear
+
 	# Root privileges test.
-	if [ "$UID" = 0 ]; then
+	if [ "$(id -u)" = 0 ]; then
 		echo "${RED}Please do not install packages from AUR using root${RESET}"
 		exit 1
 	fi
 
-	echo "${BOLD}Installing YAY...${RESET}"
-	git clone https://aur.archlinux.org/yay.git > /dev/null
+	git clone https://aur.archlinux.org/yay.git
 	cd yay && makepkg -si --noconfirm && cd - && rm -rf yay
-
-	echo "${BOLD}Installing AUR packages...${RESET}"
-	yay -S "${aur_packages[@]}" --noconfirm  > /dev/null
+	jq -r '.aur[]' software_list.json | yay -S - --needed
 }
 
 npm_install() {
-	if command -v node &>/dev/null; then
+	clear
+
+	if command -v node >/dev/null; then
 		echo "${BOLD}Installing NPM global packages..."
-		npm install -g "${npm_global_packages[@]}" > /dev/null
+		jq -r '.aur[]' software_list.json | npm install -g -
 	else
 		echo "You need to install NodeJS"
 	fi
@@ -140,75 +71,58 @@ npm_install() {
 
 dotfiles_install() {
 	clear
+
 	echo "${BOLD}Creating${RESET} ~/.config"
 	mkdir -p ~/.config
-	echo "${BOLD}Cloning repository${RESET} https://github.com/Lajck/dotfiles.git"
-	git clone https://github.com/Lajck/dotfiles.git ~/dotfiles
+	echo "${BOLD}Cloning repository${RESET} https://github.com/ludwiklejzer/.dotfiles.git"
+	git clone https://github.com/ludwiklejzer/.dotfiles.git ~/dotfiles
 	echo "${BOLD}Creating symlinks${RESET}"
-	cd ~/dotfiles && stow -vt ~ main-config  && cd -
-	sleep 1
+	cd ~/dotfiles && stow -vt ~ main-config && cd -
 }
 
-chose_install() {
-	case $INSTALLATION_NUMBER in 
-		1) 
-			clear
-			echo -e "${BOLD}${GREEN}|${RESET} ${BOLD}PACMAN${RESET}\n"
-			echo -e "${pacman_packages[*]}\n"
+confirm_installation() {
+	clear
+	printf "%s|%s %s %s\n\n" "$BOLD$GREEN" "$RESET$BOLD" "$1" "$RESET"
+	jq -r ".$1[]" software_list.json | sort | column
+	printf "\nDo you really want to install? %s[Y/n] %s" "$BOLD" "$RESET"
+	read -r KEY
+	KEY=$(echo "$KEY" | tr '[:upper:]' '[:lower:]')
+	if [ "$KEY" != "y" ] && [ "$KEY" != "" ]; then exit 1; fi
+}
 
-			read -p "Do you really want to install these packages? ${BOLD}[y/n] ${RESET}" confirm
-
-			confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
-			if [ "$confirm" != "y" ]; then exit 1; fi
-			pacman_install
+choose_install() {
+	case $NUMBER_SELECTED in
+	1)
+		confirm_installation pacman
+		pacman_install
 		;;
-		2)
-			clear
-			echo -e "${BOLD}${GREEN}|${RESET} ${BOLD}AUR${RESET}\n"
-			echo -e "${aur_packages[*]}\n"
-
-			read -p "Do you really want to install these packages? ${BOLD}[y/n] ${RESET}" confirm
-
-			confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
-			if [ "$confirm" != "y" ]; then exit 1; fi
-			aur_install
+	2)
+		confirm_installation aur
+		aur_install
 		;;
-		3)
-			clear
-			echo -e "${BOLD}${GREEN}|${RESET} ${BOLD}NPM${RESET}\n"
-			echo -e "${npm_global_packages[*]}\n"
-
-			read -p "Do you really want to install these packages? ${BOLD}[y/n] ${RESET}" confirm
-
-			confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
-			if [ "$confirm" != "y" ]; then exit 1; fi
-			npm_install
+	3)
+		confirm_installation npm
+		npm_install
 		;;
-		4)
-			clear
-			echo -e "${BOLD}${GREEN}|${RESET} ${BOLD}DOTFILES${RESET}\n"
-
-			read -p "Do you really want to set up the dotfiles? ${BOLD}[y/n] ${RESET}" confirm
-
-			confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
-			if [ "$confirm" != "y" ]; then exit 1; fi
-			dotfiles_install
+	4)
+		confirm_installation dotfiles
+		dotfiles_install
 		;;
-		*)
-			echo "Invalid Value"
-			sleep 2
-			display_options
+	*)
+		echo "Invalid Value"
+		sleep 2
+		display_options
 		;;
 	esac
 
-	echo -e "\n${BOLD}${GREEN}Done!${RESET}"
+	printf "\n%sDone!%s\n" "$BOLD$GREEN" "$RESET"
 	exit 0
 }
 
 display_options() {
 	clear
 	cat <<EOL
-${BOLD}Select a ${GREEN}NUMBER${RESET} ${BOLD}to proceed the installation of the packages${RESET}
+${BOLD}Select a ${GREEN}NUMBER${RESET} ${BOLD}to proceed the installation${RESET}
 
 ${BOLD}${GREEN}|${RESET} ${BOLD}1 - PACMAN${RESET}
 ${BOLD}${GREEN}|${RESET} ${BOLD}2 - AUR${RESET}
@@ -216,8 +130,11 @@ ${BOLD}${GREEN}|${RESET} ${BOLD}3 - NPM${RESET}
 ${BOLD}${GREEN}|${RESET} ${BOLD}4 - DOTFILES${RESET}
 
 EOL
-	read -p "${BOLD}${GREEN}>${RESET} " INSTALLATION_NUMBER
-	chose_install
+
+	printf "%s>%s " "$BOLD$GREEN" "$RESET"
+	read -r NUMBER_SELECTED
+
+	choose_install
 }
 
 display_options
