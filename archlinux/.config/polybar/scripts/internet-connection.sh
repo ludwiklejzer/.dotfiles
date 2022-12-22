419e4b2e551bebabc444d503ad4193b4d9d40ecb
Ludwik Lejzer <ludwiklejzercc@gmail.com>
2022-12-21 22:40:14 -0300

    Refactor the code

--- archlinux/.config/polybar/scripts/internet-connection.sh
+++ archlinux/.config/polybar/scripts/internet-connection.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-file_networks_list="/tmp/polybar_networks"
-nmcli device wifi list  > "$file_networks_list" # save networks in a temporary file
-sed -i '1d' "$file_networks_list" # remove file header
+NETWORK=$(nmcli -f SSID dev wifi list | sed 's/ //g;1d' - | rofi -dmenu -i -p "ESSID")
 
-networks=$(cat '/tmp/polybar_networks' | cut -c 28- | sed -r 's/\s+[a-zA-Z].+//')
-selected_network=$(echo "$networks" | rofi -dmenu -i -p "essid")
+if [ "$NETWORK" != "" ]; then
+	PASSWORD=$(rofi -show run -dmenu -password -p "Password")
+	nmcli device wifi connect "$NETWORK" password "$PASSWORD"
 
-if [ "$selected_network" != "" ]; then 
-    pass=$(rofi -show run -dmenu -password -p "Password")
-    echo "$selected_network"
-    echo "$pass"
-    nmcli device wifi connect "$selected_network" password "$pass"
-    exit_code="$?"
-    
-    if [ $exit_code -eq 0 ]; then
-        notify-send --hint=string:x-dunst-stack-tag:internet_connection "Wifi" "Connected to <i>$selected_network</i>!"
-    else
-        notify-send --hint=string:x-dunst-stack-tag:internet_connection "Wifi" "Not connected to <i>$selected_network</i>!"
-    fi
+	if "$?" -eq 0; then
+		ICON="network-wireless-hotspot"
+		dunstify -i "$ICON" -wireless -h string:x-dunst-stack-tag:internet_connection "Wifi" "Connected to <i>$NETWORK</i>!"
+	else
+		ICON="network-wireless-offline"
+		dunstify -i "$ICON" -h string:x-dunst-stack-tag:internet_connection "Wifi" "Not connected to <i>$NETWORK</i>!"
+	fi
 fi
-
