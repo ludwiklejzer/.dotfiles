583b9cdef960975d3374893df75db66118813fa0
Ludwik Lejzer <ludwiklejzercc@gmail.com>
2024-06-09 12:54:22 -0300

    Edit wezterm config file

--- archlinux/.config/wezterm/wezterm.lua
+++ archlinux/.config/wezterm/wezterm.lua
@@ -1,61 +1,70 @@
 local wezterm = require("wezterm")
+
+local catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+catppuccin.background = "#111111"
+
 local config = {
 	font = wezterm.font_with_fallback({
-		"CartographCF Nerd Font",
-		-- "JetBrainsMono Nerd Font",
+		"JetBrainsMono",
+		-- "RobotoMono Nerd Font",
+		-- "CartographCF Nerd Font",
+		-- "CaskaydiaCove Nerd Font Propo",
 		"Noto Sans CJK JP",
-	}, { weight = "Regular", style = "Normal" }),
-	font_size = 10,
-	colors = {
-		background = "#ffffff",
-		foreground = "#333333",
-		cursor_bg = "#333333",
-		cursor_fg = "#ffffff",
-		cursor_border = "#333333",
-		selection_bg = "#dae9f9",
-		-- selection_fg = "#ffffff",
-		ansi = {
-			"#000000",
-			"#cb2431",
-			"#22863a",
-			"#d0b92d",
-			"#0366d6",
-			"#9333ea",
-			"#005cc5",
-			"#ffffff",
-		},
-		brights = {
-			"#6a737d",
-			"#d73a49",
-			"#28a745",
-			"#f0c724",
-			"#2188ff",
-			"#8a63d2",
-			"#0d66d6",
-			"#ffffff",
-		},
-	},
-	line_height = 1.1,
+	}),
+	freetype_load_flags = "NO_HINTING",
+	font_size = 11.5,
+	force_reverse_video_cursor = true,
+	line_height = 1,
+	cell_width = 0.9,
+	color_schemes = { ["My Catppuccin"] = catppuccin },
+	color_scheme = "Gruvbox Material (Gogh)",
 	default_cursor_style = "SteadyBar",
 	cursor_blink_ease_out = { CubicBezier = { 0.0, 0.0, 0, 0 } },
 	cursor_blink_ease_in = { CubicBezier = { 0.0, 0.0, 0, 0 } },
 	cursor_blink_rate = 600,
 	enable_tab_bar = false,
 	window_background_opacity = 1,
-	window_padding = {
-		-- left = 20,
-		-- right = 20,
-		-- top = 20,
-		-- bottom = 20,
-
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+	window_padding = {},
 	adjust_window_size_when_changing_font_size = false,
 	keys = {
 		{ key = "Space", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+		{
+			key = "Enter",
+			mods = "ALT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+		{
+			key = "7",
+			mods = "CTRL",
+			action = wezterm.action.Multiple({
+				wezterm.action.SendKey({ key = "v" }),
+				wezterm.action.SendKey({ key = "Enter" }),
+			}),
+		},
+		{
+			key = "8",
+			mods = "CTRL",
+			action = wezterm.action.Multiple({
+				wezterm.action.SendKey({ key = "Space" }),
+				wezterm.action.SendKey({ key = "w" }),
+				wezterm.action.SendKey({ key = "w" }),
+			}),
+		},
+	},
+	window_background_gradient = {
+		-- Can be "Vertical" or "Horizontal".  Specifies the direction
+		-- in which the color gradient varies.  The default is "Horizontal",
+		-- with the gradient going from left-to-right.
+		-- Linear and Radial gradients are also supported; see the other
+		-- examples below
+		orientation = "Vertical",
+		colors = {
+			"rgba(27,27,27, 1)",
+			"rgba(27,27,27, 0.65)",
+		},
+		interpolation = "Linear",
+		blend = "Rgb",
+		noise = 64,
 	},
 }
 
